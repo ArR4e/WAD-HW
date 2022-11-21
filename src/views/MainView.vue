28a51257911587ec3fc972fa6914240ef5e2c525
NickPoint <nikita.voevudsky@gmail.com>
2022-11-21 23:41:13 +0200

    Styles for the reset button are added

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <post v-for="post of posts" :post="post"/>
-    <button @click="resetLikes">Reset likes</button>
+    <div id="button-container">
+      <button @click="resetLikes">Reset likes</button>
+    </div>
   </div>
 </template>
 
@@ -43,6 +45,35 @@ html {
 main {
   margin-top: 60px;
   margin-inline: 25%;
+}
+
+#button-container{
+  margin: 50px 0;
+  text-align: center;
+}
+
+#button-container button {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  font-weight: 700;
+  font-size: 1em;
+  color: #03e9f4;
+  transition: .5s;
+  letter-spacing: 4px;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-family: "Roboto", sans-serif;
+}
+#button-container button:hover{
+  background: #03e9f4;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #03e9f4,
+  0 0 25px #03e9f4,
+  0 0 50px #03e9f4,
+  0 0 100px #03e9f4;
 }
 
 </style>
